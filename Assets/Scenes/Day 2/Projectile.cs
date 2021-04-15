d1aa2104457fd0db690ef8641f1845a9f7a8693f
Isabella Herman <isavbherman@gmail.com>
2021-04-15 10:23:17 -0300

    Conteudo dia 02

---
+++ Assets/Scenes/Day 2/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    //private Vector2 target; //Vector2 (x,y)
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //target = new Vector2(player.position.x, player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);                
+
+        if(transform.position.x == player.position.x && transform.position.y == player.position.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    public void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
